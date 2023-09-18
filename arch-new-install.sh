ac0ecc3805178e72fdd17d7d2bb3ae217c65587a
Archisman Panigrahi <apandada1@gmail.com>
2023-09-18 19:48:01 -0400

    Create arch-new-install.sh

---
+++ arch-new-install.sh
@@ -0,0 +1,25 @@
+sudo pacman -S --needed git base-devel
+git clone https://aur.archlinux.org/yay-bin.git
+cd yay-bin
+sudo makepkg -si
+
+### KDE Specific
+
+yay -S plasma-desktop  plasma-wayland-session sddm
+sudo systemctl enable sddm.service
+
+yay -S okular gwenview partitionmanager kalzium kamoso kdeconnect latte-dock filelight ksysguard 
+
+yay -S inkscape foliate blanket wike vlc firefox chromium telegram-desktop octave jupyter-notebook code freetube-bin geogebra github-desktop-bin globalprotect-openconnect marker kvantum texlive-basic texlive-latexextra texlive-latexrecommended texlive-mathscience texlive-plaingeneric texlive-xetex texstudio pdfarranger libreoffice-fresh micro
+
+yay -S openblas-lapack
+yay -S gimp-devel fcitx5-openbangla-git joplin-appimage noto-fonts-emoji-apple etcher-bin annotator insect julia-bin julia-bin megasync-bin dropbox stellarium-bin parabolic tex-match-bin boop-gtk-bin zoom simplescreenrecorder
+
+## Afterwards, add appropriate lines to `/etc/environment` from https://github.com/archisman-panigrahi/dotfiles.git.
+yay -S wget
+wget https://raw.githubusercontent.com/archisman-panigrahi/dotfiles/main/arch-etc-environment-kde-wayland
+sudo mv arch-etc-environment-kde-wayland /etc/environment
+wget https://raw.githubusercontent.com/archisman-panigrahi/dotfiles/main/endeavouros%3C--Ubuntu.bashrc
+mv endeavouros\<--Ubuntu.bashrc ~/.bashrc
+
+## Then overwrite `$HOME/.config` and `$HOME/.local`
