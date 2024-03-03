9befe80c7f2f1155cb673b5c37c57bf79205c20a
Archisman Panigrahi <apandada1@gmail.com>
2024-03-03 17:08:05 -0500

    Update arch-new-install.sh

--- arch-new-install.sh
+++ arch-new-install.sh
@@ -9,7 +9,8 @@ sudo pacman -S plasma-desktop  plasma-wayland-session sddm
 sudo systemctl enable sddm.service
 
 sudo pacman -S okular gwenview partitionmanager kalzium kamoso kdeconnect latte-dock filelight ksysguard 
-
+## KDE Connect filesystem browsing
+sudo pacman -S sshfs
 sudo pacman -S inkscape foliate blanket wike vlc firefox chromium telegram-desktop octave jupyter-notebook code geogebra globalprotect-openconnect marker kvantum texlive-basic texlive-latexextra texlive-latexrecommended texlive-mathscience texlive-plaingeneric texlive-xetex texstudio pdfarranger libreoffice-fresh libreoffice-extension-texmaths micro remmina hunspell hunspell-en_us fastfetch ttf-junicode noto-fonts-emoji kdegraphics-thumbnailers
 yay -S openblas-lapack
 yay -S gimp-devel fcitx5-openbangla-git joplin-appimage noto-fonts-emoji-apple etcher-bin annotator numbat-bin julia-bin megasync-bin dropbox stellarium-bin parabolic tex-match-bin boop-gtk-bin zoom simplescreenrecorder freetube-bin github-desktop-bin
