c01624c054ccea297a5e2ac803b766627b710e30
Archisman Panigrahi <apandada1@gmail.com>
2024-03-04 07:31:45 -0500

    Add gh tool

--- mint-first-install.sh
+++ mint-first-install.sh
@@ -49,7 +49,13 @@ sudo apt full-upgrade -y
 cd /tmp
 wget https://github.com/ful1e5/Google_Cursor/releases/download/v2.0.0/GoogleDot-Red.tar.gz
 tar -xvf GoogleDot-Red.tar.gz
-sudo mv GoogleDot-* /usr/share/icons/
+sudo mv GoogleDot-Red /usr/share/icons/
+
+sudo mkdir -p -m 755 /etc/apt/keyrings && wget -qO- https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo tee /etc/apt/keyrings/githubcli-archive-keyring.gpg > /dev/null \
+&& sudo chmod go+r /etc/apt/keyrings/githubcli-archive-keyring.gpg \
+&& echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
+&& sudo apt update \
+&& sudo apt install gh -y
 
 sudo apt clean
 sudo apt autoremove -y
