9e742d62585620efa84ea5201131caad0d4baac5
Archisman Panigrahi <apandada1@gmail.com>
2024-03-05 00:45:25 -0500

    Create okular_foliate_sync.sh

---
+++ okular_foliate_sync.sh
@@ -0,0 +1,3 @@
+ln -s  /run/media/Store/Mega/foliate_progress/com.github.johnfactotum.Foliate /home/archisman/.local/share/com.github.johnfactotum.Foliate
+ln -s /run/media/Store/Mega/okular_docdata/ /home/archisman/.local/share/okular
+
