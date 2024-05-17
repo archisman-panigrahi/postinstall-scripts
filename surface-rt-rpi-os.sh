b5a65b2bdb03f1bf9a80cef702b5b90225068c05
Archisman Panigrahi <apandada1@gmail.com>
2024-05-16 23:17:11 -0400

    Update surface-rt-rpi-os.sh

--- surface-rt-rpi-os.sh
+++ surface-rt-rpi-os.sh
@@ -1,10 +1,10 @@
 sudo apt update
-sudo apt install git
+sudo apt install git -y
 
 ## twofing
 cd /tmp
 git clone https://github.com/plippo/twofing
-sudo apt-get install build-essential libx11-dev libxtst-dev libxi-dev x11proto-randr-dev libxrandr-dev xserver-xorg-input-evdev xserver-xorg-input-evdev-dev
+sudo apt install -y build-essential libx11-dev libxtst-dev libxi-dev x11proto-randr-dev libxrandr-dev xserver-xorg-input-evdev xserver-xorg-input-evdev-dev
 cd twofing
 make
 sudo make install
@@ -48,7 +48,7 @@ wget -qO- https://raw.githubusercontent.com/Botspot/pi-apps/master/install | bas
 ## Numbat
 cd /tmp
 wget https://github.com/sharkdp/numbat/releases/download/v1.11.0/numbat_1.11.0_armhf.deb
-sudo dpkg -i numbat_1.11.0_armhf.deb
+sudo dpkg -i numbat_1.11.0_armhf.deb -y
 
 ## Julia
 cd
@@ -57,8 +57,8 @@ curl -fsSL https://install.julialang.org | sh
 ## Marker
 cd /tmp
 wget https://launchpad.net/~apandada1/+archive/ubuntu/marker/+files/marker_2023.05.02-1~202304282146~ubuntu22.04.1_armhf.deb
-sudo apt install ./marker_2023.05.02-1~202304282146~ubuntu22.04.1_armhf.deb
+sudo apt install -y ./marker_2023.05.02-1~202304282146~ubuntu22.04.1_armhf.deb
 
 ## General software from the repositories
-sudo apt install okular --no-install-recommends
-sudo apt install foliate
+sudo apt install -y okular --no-install-recommends
+sudo apt install -y foliate
