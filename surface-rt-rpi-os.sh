750452a7897843e9b85fe08daf1ec9cc7dde6da3
Archisman Panigrahi <apandada1@gmail.com>
2024-05-16 22:39:21 -0400

    Update surface-rt-rpi-os.sh

--- surface-rt-rpi-os.sh
+++ surface-rt-rpi-os.sh
@@ -19,6 +19,28 @@ git clone https://github.com/archisman-panigrahi/surface-RT-screen-rotator
 cd surface-RT-screen-rotator
 bash setup.sh
 
+## Brightness Script
+pip3 install --user rpi-backlight --break-system-packages
+echo 'SUBSYSTEM=="backlight",RUN+="/bin/chmod 666 /sys/class/backlight/%k/brightness /sys/class/backlight/%k/bl_power"' | sudo tee -a /etc/udev/rules.d/backlight-permissions.rules
+TARGET_DIR="$HOME/.local/share/applications"
+TARGET_FILE="$TARGET_DIR/rpi-backlight.desktop"
+mkdir -p "$TARGET_DIR"
+cd "$TARGET_DIR"
+
+cat > "$TARGET_FILE" <<EOL
+[Desktop Entry]
+Version=1.0
+Type=Application
+Terminal=false
+Name=rpi-backlight GUI
+Exec=rpi-backlight-gui
+Icon=/usr/share/icons/Adwaita/symbolic/status/display-brightness-symbolic.svg
+Categories=Utility;
+EOL
+
+# Make the desktop entry file executable
+chmod +x "$TARGET_FILE"
+
 ## Pi Apps
 cd
 wget -qO- https://raw.githubusercontent.com/Botspot/pi-apps/master/install | bash
