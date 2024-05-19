cfb6583242fd67bbe25290f053503ceabfdc609d
Archisman Panigrahi <apandada1@gmail.com>
2024-05-19 01:07:01 -0400

    Update surface-rt-rpi-os.sh

--- surface-rt-rpi-os.sh
+++ surface-rt-rpi-os.sh
@@ -4,41 +4,7 @@
 ### Instead run it with `bash surface-rt-rpi-os.sh`
 
 sudo apt update
-sudo apt install git -y
-
-## twofing
-cd /tmp
-git clone https://github.com/plippo/twofing
-sudo apt install -y build-essential libx11-dev libxtst-dev libxi-dev x11proto-randr-dev libxrandr-dev xserver-xorg-input-evdev xserver-xorg-input-evdev-dev
-cd twofing
-make
-sudo make install
-
-## Screen Orientation Manager
-cd
-git clone https://github.com/archisman-panigrahi/surface-RT-screen-rotator
-cd surface-RT-screen-rotator
-bash setup.sh
-cp $HOME/.local/share/applications/surface-rt-screen-rotator.desktop $HOME/.config/autostart/
-
-## Brightness Script
-pip3 install --user rpi-backlight --break-system-packages
-echo 'SUBSYSTEM=="backlight",RUN+="/bin/chmod 666 /sys/class/backlight/%k/brightness /sys/class/backlight/%k/bl_power"' | sudo tee -a /etc/udev/rules.d/backlight-permissions.rules
-TARGET_DIR="$HOME/.local/share/applications"
-TARGET_FILE="$TARGET_DIR/rpi-backlight.desktop"
-mkdir -p "$TARGET_DIR"
-cd "$TARGET_DIR"
-
-cat > "$TARGET_FILE" <<EOL
-[Desktop Entry]
-Version=1.0
-Type=Application
-Terminal=false
-Name=rpi-backlight GUI
-Exec=rpi-backlight-gui
-Icon=/usr/share/icons/Adwaita/scalable/status/display-brightness-symbolic.svg
-Categories=Utility;
-EOL
+sudo apt install git wget -y
 
 # Make the desktop entry file executable
 chmod +x "$TARGET_FILE"
@@ -82,6 +48,11 @@ cd xf86-video-opentegra
 ./autogen.sh
 make -j4
 sudo make install
+
+### Brightness and Screen Rotation Script
+cd /tmp
+wget https://raw.githubusercontent.com/archisman-panigrahi/postinstall-scripts/main/srt-brightness-screen-rotation.sh
+bash srt-brightness-screen-rotation.sh
 
 ## General software from the repositories
 sudo apt install -y okular --no-install-recommends
