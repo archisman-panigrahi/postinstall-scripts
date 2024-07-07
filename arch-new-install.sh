d84c48ecbef323f21f5de0931603ed9412fa4b0b
Archisman Panigrahi <apandada1@gmail.com>
2024-07-07 09:37:25 -0400

    Update arch-new-install.sh

--- arch-new-install.sh
+++ arch-new-install.sh
@@ -13,7 +13,7 @@ sudo pacman -S okular gwenview partitionmanager kalzium kamoso kdeconnect latte-
 sudo pacman -S sshfs
 sudo pacman -S inkscape foliate blanket wike vlc firefox chromium telegram-desktop octave jupyter-notebook code geogebra globalprotect-openconnect marker kvantum texlive-basic texlive-latexextra texlive-latexrecommended texlive-mathscience texlive-plaingeneric texlive-xetex texstudio pdfarranger libreoffice-fresh libreoffice-extension-texmaths micro remmina hunspell hunspell-en_us fastfetch ttf-junicode noto-fonts-emoji kdegraphics-thumbnailers
 yay -S openblas-lapack
-yay -S gimp-devel fcitx5-openbangla-git joplin-appimage noto-fonts-emoji-apple etcher-bin annotator numbat-bin julia-bin megasync-bin dropbox stellarium-bin parabolic tex-match-bin boop-gtk-bin zoom simplescreenrecorder freetube-bin github-desktop-bin
+yay -S gimp-devel fcitx5-openbangla-git joplin-appimage noto-fonts-emoji-apple etcher-bin annotator numbat-bin julia-bin megasync-bin dropbox stellarium-bin parabolic tex-match-bin boop-gtk-bin zoom simplescreenrecorder freetube-bin github-desktop-bin dolphin-plugins dolphin-megasync-bin
 
 ## Create a mock neofetch wrapper for fastfetch, because I remember neofetch more
 sudo ln -s /usr/bin/fastfetch /usr/bin/neofetch
