6f330e972351f26a775bebb94576426609f39c07
Archisman Panigrahi <apandada1@gmail.com>
2024-09-11 09:27:54 -0400

    Update arch-new-install.sh

--- arch-new-install.sh
+++ arch-new-install.sh
@@ -8,12 +8,15 @@ sudo makepkg -si
 sudo pacman -S plasma-desktop  plasma-wayland-session sddm
 sudo systemctl enable sddm.service
 
-sudo pacman -S okular gwenview partitionmanager kalzium kamoso kdeconnect latte-dock filelight ksysguard 
+sudo pacman -S okular gwenview partitionmanager kalzium kamoso kdeconnect latte-dock filelight ksysguard kdegraphics-thumbnailers
 ## KDE Connect filesystem browsing
 sudo pacman -S sshfs
-sudo pacman -S inkscape foliate blanket wike vlc firefox chromium telegram-desktop octave jupyter-notebook code geogebra globalprotect-openconnect marker kvantum texlive-basic texlive-latexextra texlive-latexrecommended texlive-mathscience texlive-plaingeneric texlive-xetex texstudio pdfarranger libreoffice-fresh libreoffice-extension-texmaths micro remmina hunspell hunspell-en_us fastfetch ttf-junicode noto-fonts-emoji kdegraphics-thumbnailers
+sudo pacman -S inkscape foliate blanket wike vlc firefox chromium telegram-desktop octave jupyter-notebook code geogebra globalprotect-openconnect marker kvantum texlive-basic texlive-latexextra texlive-latexrecommended texlive-mathscience texlive-plaingeneric texlive-xetex texstudio pdfarranger libreoffice-fresh libreoffice-extension-texmaths micro remmina hunspell hunspell-en_us fastfetch ttf-junicode noto-fonts-emoji nvtop
+sudo pacman -S plocate
+sudo updatedb
+
 yay -S openblas-lapack
-yay -S gimp-devel fcitx5-openbangla-git joplin-appimage noto-fonts-emoji-apple etcher-bin annotator numbat-bin julia-bin megasync-bin dropbox stellarium-bin parabolic tex-match-bin boop-gtk-bin zoom simplescreenrecorder freetube-bin github-desktop-bin dolphin-plugins dolphin-megasync-bin
+yay -S gimp-devel fcitx5-openbangla-git joplin-appimage noto-fonts-emoji-apple etcher-bin annotator numbat-bin julia-bin megasync-bin dropbox stellarium-bin parabolic hieroglyphic-bin boop-gtk-bin zoom simplescreenrecorder freetube-bin github-desktop-bin dolphin-plugins dolphin-megasync-bin
 
 ## Create a mock neofetch wrapper for fastfetch, because I remember neofetch more
 sudo ln -s /usr/bin/fastfetch /usr/bin/neofetch
