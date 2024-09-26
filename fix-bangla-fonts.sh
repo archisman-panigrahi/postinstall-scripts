a1f94654fee12c36b026bd0d7b8d43728da7649b
Archisman Panigrahi <apandada1@gmail.com>
2024-09-26 09:45:31 -0400

    Create fix-bangla-fonts.sh

---
+++ fix-bangla-fonts.sh
@@ -0,0 +1,4 @@
+sudo cp $PWD/fonts/NotoSans-Regular.otf /usr/share/fonts/opentype/freefont/FreeSans.otf
+sudo cp $PWD/fonts/NotoSans-Regular.ttf /usr/share/fonts/truetype/freefont/FreeSans.ttf
+sudo cp $PWD/fonts/NotoSans-Bold.otf /usr/share/fonts/opentype/freefont/FreeSansBold.otf
+sudo cp $PWD/fonts/NotoSans-Bold.ttf /usr/share/fonts/truetype/freefont/FreeSansBold.ttf
