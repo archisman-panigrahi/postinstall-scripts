64ea30a7b66f138bd2516abf2a741f395017b7e3
Archisman Panigrahi <apandada1@gmail.com>
2024-11-30 18:26:30 -0500

    Update mint-first-install.sh

--- mint-first-install.sh
+++ mint-first-install.sh
@@ -24,6 +24,8 @@ rm -f packages.microsoft.gpg
 wget -qO - https://apt.packages.shiftkey.dev/gpg.key | gpg --dearmor | sudo tee /usr/share/keyrings/shiftkey-packages.gpg > /dev/null
 sudo sh -c 'echo "deb [arch=amd64 signed-by=/usr/share/keyrings/shiftkey-packages.gpg] https://apt.packages.shiftkey.dev/ubuntu/ any main" > /etc/apt/sources.list.d/shiftkey-packages.list'
 
+gsettings set org.cinnamon.desktop.wm.preferences button-layout "close,minimize,maximize:"
+
 sudo apt update
 
 
